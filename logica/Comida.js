639892201c66a68091bcf17772636887d6942ef7
MaikyYeaaa <kn.maiky@gmail.com>
2023-08-04 13:06:10 -0300

    abl productos casi terminado

--- logica/Comida.js
+++ logica/Comida.js
@@ -19,9 +19,8 @@ fetch("../../persistencia/listarComidas.php")
             `;
       $("#listado-platos").append(mostrar);
     }
-    let productos = Array.from($(".producto"));
-    console.log(productos);
 
+    let productos = Array.from($(".producto")); //los hago array para trabajar con todos ellos
     productos.forEach((producto) => {
       producto.addEventListener("click", function () {
         let idcomida = $(producto).attr("data-idComida");
@@ -38,7 +37,7 @@ fetch("../../persistencia/listarComidas.php")
 
                 mostrar = `
                 <img id="cerrar" src="../../src/cross.svg" alt="" />
-                <h1>${nombre} $400</h1>
+                <h1>${nombre} </h1>
                 <img
                 id="productoImg"
                 src="${img}"
@@ -64,35 +63,76 @@ fetch("../../persistencia/listarComidas.php")
 $("#btnFiltroComida").click(filtrar);
 
 function filtrar() {
-  $("#mostrar").html("");
-  let filtro = $("#txtFiltro").val();
+  let filtro = $("#txtFiltroComida").val();
+  let filtroCorrecto = false;
+  console.log(filtro);
   fetch("../../persistencia/listarComidas.php")
     .then((r) => r.json())
     .then((r) => {
       r.forEach((r) => {
         let id_comida = r.IDComida;
         let nombre_comida = r.Nombre;
-        let descripcion = r.Descripcion;
         let imgURL = r.ImagenURL;
 
         if (filtro == id_comida || filtro == nombre_comida) {
+          $("#listado-platos").html("");
+          filtroCorrecto = true;
           mostrar = `
-          <article>
+          <article class="producto" id="#comida" data-idComida="${id_comida}">
+          <section id="fondo">
+            <img
+              src="${imgURL}"
+              alt=""
+            />
+          </section>
           <h1>${nombre_comida}</h1>
-          <p>Descripcion: ${descripcion}</p>
-          <image src="${imgURL}" width="200px">
-          <form id="eliminar">
-          <input type="submit" value="Eliminar" name="${id_comida}" data-nombre="${nombre_comida}"/>
-        </form>
         </article>
-            `;
-          $("#mostrar").append(mostrar);
+                `;
+          $("#listado-platos").append(mostrar);
         }
       });
-      let valorMostrar = $("#mostrar").val();
-      console.log(valorMostrar);
-      if (valorMostrar == " ") {
-        $("#mostrar").html(`no hay nada registrado como ${filtro}`);
+      if (!filtroCorrecto) {
+        alert(`no hay nada registrado como ${filtro}`);
+      } else {
+        let productos = Array.from($(".producto"));
+
+        productos.forEach((producto) => {
+          producto.addEventListener("click", function () {
+            let idcomida = $(producto).attr("data-idComida");
+            console.log(idcomida);
+            fetch("../../persistencia/listarComidas.php")
+              .then((r) => r.json())
+              .then((r) => {
+                r.forEach((r) => {
+                  if (idcomida == r.IDComida) {
+                    console.log(r.IDComida);
+                    let nombre = r.Nombre;
+                    let desc = r.Descripcion;
+                    let img = r.ImagenURL;
+
+                    mostrar = `
+                    <img id="cerrar" src="../../src/cross.svg" alt="" />
+                    <h1>${nombre}</h1>
+                    <img
+                    id="productoImg"
+                    src="${img}"
+                    />
+                    <p>
+                    ${desc}
+                    </p>
+                    <section id="btnContenedor">
+                    <form id="eliminar">
+                    <input class="btnSecundario" type="submit" value="Eliminar" name="${idcomida}" data-nombre="${nombre}"/>
+                  </form>
+                    </section>
+                    `;
+                    $("#modal #modal-content").html(mostrar);
+                    $("#modal").css({ visibility: "visible" });
+                  }
+                });
+              });
+          });
+        });
       }
     });
 }
