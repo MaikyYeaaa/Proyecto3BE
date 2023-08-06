0d00649989e37d0d5a6a5c6373c5304ee27cdde5
MaikyYeaaa <kn.maiky@gmail.com>
2023-08-05 22:05:10 -0300

    login listo!!
    Co-Authored-By: gerss12345 <41414172+gerss12345@users.noreply.github.com>

---
+++ persistencia/login.php
@@ -0,0 +1,21 @@
+<?php
+require "helperFunctions.php";
+
+$con = conectarBDD();
+
+$mail = mysqli_real_escape_string($con, $_POST["mail"]); 
+$contra = mysqli_real_escape_string($con, $_POST["cont"]);
+
+$sql = "SELECT `Contrasena`, `Mail` FROM `usuario` WHERE Mail = '${mail}'";
+if($userData = getFromBDD($sql, $con)) { // $userData = [{"Contrasena":"jernacioContra336969","Mail":"jernaaaa@gmail.com"}]
+   if($userData[0]["Contrasena"] == $contra) {
+    echo "ningunError";
+   } else {
+    echo "contraError";
+   }
+} else {
+    echo "mailError";
+}
+
+// echo json_encode($userData);
+?>
