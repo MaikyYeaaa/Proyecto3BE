0770766aac6c30ae652ec5fe7986330c4461df60
GermanSendic <41414172+gerss12345@users.noreply.github.com>
2023-10-16 03:06:18 -0300

    InternalFinalizado

---
+++ scripts/navIntBtn.js
@@ -0,0 +1,16 @@
+let selected = false;
+
+$(document).ready(function() {
+    const $section = $('#adminMenu');
+    $("#intBtn").click(function() {
+        if(selected){
+            $('#adminMenu').css('display', 'none');
+        }else{
+            $('#adminMenu').css('display', 'block');
+        }
+        selected = !selected;
+      
+      console.log("Holanda");
+    });
+      
+  });
