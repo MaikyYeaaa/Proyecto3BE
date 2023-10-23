bb4be06db1cd593ea9d79e96c50e2cae1a708c7b
GermanSendic <41414172+gerss12345@users.noreply.github.com>
2023-10-23 16:06:50 -0300

    CustomMenuCss

--- testcustommenu.js
+++ testcustommenu.js
@@ -9,3 +9,43 @@ document.querySelectorAll('#menuBody').forEach(function (menuBody) {
 document.querySelector('.close-modal').addEventListener('click', function () {
     document.getElementById('menuModal').style.display = 'none';
 });
+
+$("#menuAddBtn").click(addMenu);
+
+function addMenu(){
+    document.getElementById('menuModal').style.display = 'none';
+}
+
+
+const scrollContainers = document.querySelectorAll('.scroll-container');
+
+function scrollRight(scrollContainer) {
+    scrollContainer.scrollLeft += 1; // Ajusta la velocidad de desplazamiento aquí
+    if (scrollContainer.scrollLeft < scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+        requestAnimationFrame(() => scrollRight(scrollContainer));
+    } else {
+        setTimeout(() => scrollLeft(scrollContainer), 2000);
+    }
+}
+
+function scrollLeft(scrollContainer) {
+    scrollContainer.scrollLeft -= 1; // Ajusta la velocidad de desplazamiento aquí
+    if (scrollContainer.scrollLeft > 0) {
+        requestAnimationFrame(() => scrollLeft(scrollContainer));
+    } else {
+        setTimeout(() => scrollRight(scrollContainer), 2000);
+    }
+}
+
+scrollContainers.forEach((scrollContainer) => {
+    scrollContainer.addEventListener('mouseenter', () => {
+        cancelAnimationFrame(() => scrollLeft(scrollContainer));
+        cancelAnimationFrame(() => scrollRight(scrollContainer));
+    });
+
+    scrollContainer.addEventListener('mouseleave', () => {
+        setTimeout(() => scrollRight(scrollContainer), 2000);
+    });
+
+    setTimeout(() => scrollRight(scrollContainer), 2000);
+});
